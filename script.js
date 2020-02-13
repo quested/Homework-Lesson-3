a3d9389c9c2195b0339e2c64d427f7d3556341a3
Edik Deineka <e.v.deineka@gmail.com>
2020-02-13 20:53:55 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+var str = 'adfsdfdsaffasfaaaaasfsfasfdsf';
+var n = 'a';
+var result = 0;
+var i = 0;
+
+
+
+for (i = 0; i <= str.length; i++) {
+    if (str.charAt(i) == n) {
+        result ++;
+    } 
+} 
+console.log(result);
+
+      
+
+while (i <= str.length) {
+    if (str.charAt(i) == n) {
+        result++;
+    } i++;
+} 
+console.log(result);
+
+
+
+do {if (str.charAt(i) == n) {
+    result++;
+    } i++;
+} while (i <= str.length);
+console.log(result);
+  
+      
+
+function letterStr() {
+    result = 0;
+    for (i = 0; i <= str.length; i++) {
+        if (str.charAt(i) == n) {
+            result ++;
+        }
+    } return result;
+}
+console.log(letterStr("adfsdfdsaffasfaaaaasfsfasfdsf", "a"));
